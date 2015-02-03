5b4e0b39699d84470bdab555cd8f5a3be1ef6fa9
Brad Habenicht <brad.habenicht@gmail.com>
2015-02-03 01:45:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BfhSplurty::Application.config.secret_key_base = 'fa5d4230452fb33f32955b4e8c55741cd4f23c6c3d4e2b641bba4b18fcb2afaebc745aa64ec5759c0fccfd52fb4345ee49879eaf6fc51c9c6518e596d27d79a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BfhSplurty::Application.config.session_store :cookie_store, key: '_bfh_splurty_session'
